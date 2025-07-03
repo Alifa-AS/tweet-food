325a1690426a62a5df905c6023ea71530d65f196
AlifaSrn <alifa.nub@gmail.com>
2025-07-04 00:49:15 +0600

    added some thing

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,7 @@
-import { Poppins, Lobster_Two} from "next/font/google";
+import { Poppins, Lobster_Two } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -12,7 +11,7 @@ const poppins = Poppins({
 const lobster = Lobster_Two({
   subsets: ["latin"],
   weight: ["400"],
-})
+});
 
 export const metadata = {
   title: "Next Food App",
@@ -22,12 +21,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${poppins.className}`}>
+      <body className={`${poppins.className} bg-[#fdf0e9] min-h-screen`}>
         <div className="w-full max-w-[1440px] mx-auto px-4 sm:px-6 lg:px-8">
-          <Navbar />
-            {children}
-            <Footer />
-          </div>
+          <Navbar /> 
+          {children}
+          <Footer />
+        </div>
       </body>
     </html>
   );
